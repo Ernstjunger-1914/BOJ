771e64e209745e76615cf01736c937b687bb365b
kgh <vot768@naver.com>
2021-12-12 22:42:44 +0900

    21533번 : 암호 만들기

---
+++ 21553/21553.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main() {
+  fastio;
+
+  string a, p;
+  cin>>a>>p;
+  cout<<p<<"\n";
+
+  return 0;
+}
---
+++ 21553/README.md
@@ -0,0 +1,5 @@
+# Problem
+[암호 만들기](https://www.acmicpc.net/problem/21553)
+   
+# 문제 해설
+b 와 p 가 같다면 다른 암호가 나올 수 없기에 조건을 만족한다.   
