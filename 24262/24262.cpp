b8efbee5e1694d6ebb141da507a8c7759e81e491
kgh <vot768@naver.com>
2022-02-06 20:58:37 +0900

    24262번 : 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ 24262/24262.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ioboost ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+using ll=long long;
+
+int main() {
+  ioboost;
+
+  cout<<1<<"\n"<<0<<"\n";
+
+  return 0;
+}
