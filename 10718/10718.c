88757d9df49691dd45ef18be13d6e21307d29d05
손소독 <vot768@naver.com>
2022-02-16 19:07:34 +0900

    10718번 : We love kriii

---
+++ 10718/10718.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	char output[100]="강한친구 대한육군";
+
+	printf("%s\n", output);
+	printf("%s\n", output);
+	
+	return 0;
+}
---
+++ 10718/README.md
@@ -0,0 +1,2 @@
+# Problem
+[We love kriii](https://www.acmicpc.net/problem/10718)
